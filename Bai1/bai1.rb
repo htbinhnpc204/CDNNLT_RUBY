02f8c1ef82f2f6ad192cdb5c754545e411f6397f
binh ho <htbinhnpc204@gmail.com>
2022-08-27 11:33:03 +0700

    Create project & done Ex1

---
+++ Bai1/bai1.rb
@@ -0,0 +1,12 @@
+puts "Xin chào: Hồ Thái Bình - 1911505310105"
+
+puts "Nhâp vào 1 số: "
+num = gets
+puts "Số vừa nhập là: #{num}"
+
+puts "Nhập vào 2 số để so sánh: "
+print "Số thứ nhất: "
+a = gets
+print "Số thứ hai: "
+b = gets
+puts"Số lớn hơn là: #{a > b ? a : b}"
